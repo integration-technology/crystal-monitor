cf572d8d70630d2e8576dee14d2d0be3823abb78
owain <owain@intergration.technology>
2022-08-03 20:01:23 +0200

    added in events from Airtable and tested

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {monthDiff} from './lib/utils'
-import {api, params, schedule} from "@serverless/cloud"
+import {api, events, schedule} from "@serverless/cloud"
 import {fetchBookings} from "./lib/booking";
 
 // Create GET route and return number of months from departure
@@ -18,11 +18,17 @@ api.get("/months", async (req, res) => {
 })
 
 
-schedule.every("1 hour", () => {
+schedule.every("1 hour", async () => {
     // This code block will run every hour!
-    console.log("I run every hour!")
+    console.log("I run every hour! Fetching the bookings from Airtable and publishing individually as events with a timestamp")
+    const bookings = await fetchBookings()
+    await Promise.all(bookings.map((booking) => {
+        return events.publish('booking.scrape.request', {...booking, timestamp: new Date().toISOString()})
+    }))
 })
 
-
+events.on("booking.scrape.request", async ({body}) => {
+    console.info('booking.scrape.request', body)
+});
 
 
