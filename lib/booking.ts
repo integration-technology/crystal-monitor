62bf62c28513d3034a627c9e9619aa6463030bd5
owain <owain@intergration.technology>
2022-08-03 16:42:40 +0200

    tested only return the necessary booking properties

--- lib/booking.ts
+++ lib/booking.ts
@@ -10,13 +10,13 @@ export async function fetchBookings() {
 
 // Initialize a base
     const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
-// Reference a table
-    const bookings = await base('Booking').select({
+// Reference a table and get all records
+     const bookings =  await base('Booking').select({
         // Selecting the first 3 records in Grid view:
-        maxRecords: 50,
-        view: "Grid view",
         fields: ["Reference", "Surname", "Departure Date"],
         sort: [{field: "Reference", direction: "asc"}]
     }).all()
-    return bookings
+    return bookings.map((record) => {
+        return record.fields
+    })
 }
--- tests/unit/booking.test.ts
+++ tests/unit/booking.test.ts
@@ -1,6 +1,16 @@
 import {fetchBookings} from "../../lib/booking"
 
 test("Test able to connect to base", async () => {
-    const result = await fetchBookings()
-    expect(result).toHaveLength(7)
+    const bookings = await fetchBookings()
+    expect(bookings).toHaveLength(7)
+    expect(bookings[0]).toHaveProperty('Departure Date')
+    expect(bookings[0]).toHaveProperty('Reference')
+})
+
+test("Contain the correct booking Properties", async () => {
+    const bookings = await fetchBookings()
+    const booking = bookings[0]
+    expect(booking).toHaveProperty('Departure Date')
+    expect(booking).toHaveProperty('Reference')
+    expect(booking).toHaveProperty('Surname')
 })
