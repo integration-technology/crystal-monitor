cd6e9fd9b34ab022ff0cdb42917ad96af3947eb1
owain <owain@intergration.technology>
2022-08-03 16:44:41 +0200

    user SC params in preference to process.env

--- lib/booking.ts
+++ lib/booking.ts
@@ -1,15 +1,15 @@
-import {params} from "@serverless/cloud"
+import { params } from "@serverless/cloud"
 
-import Airtable, {base} from "airtable"
+import Airtable from "airtable"
 
 export async function fetchBookings() {
 
     Airtable.configure({
-        apiKey: process.env.AIRTABLE_API_KEY,
+        apiKey: params.AIRTABLE_API_KEY || process.env.AIRTABLE_API_KEY,
     });
 
 // Initialize a base
-    const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
+    const base = Airtable.base(params.AIRTABLE_BASE_ID || process.env.AIRTABLE_BASE_ID)
 // Reference a table and get all records
      const bookings =  await base('Booking').select({
         // Selecting the first 3 records in Grid view:
